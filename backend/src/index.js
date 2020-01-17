e2d6b99f5d0648dadee47345008d13b001ef3bca
Maicon Mares <maiconmaresunb@gmail.com>
2020-01-17 11:35:44 -0300

    Adicionando Axios.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express'); //require() busca o pacote express em /node_modules
 const mongoose = require('mongoose'); //require() busca o pacote express em /node_modules
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 
 app.get('/users', (request, response) => {
