707796c359c09ccaa5aa5ab62b0d0b6ce6893b46
Maicon Mares <maiconmaresunb@gmail.com>
2020-01-17 10:03:09 -0300

    Criando pagina inicial da aplicacao.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,93 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Teste from'./Teste';
+import './global.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function incrementCounter() {
-    setCounter(counter + 1);
-  }
-
-  function decrementCounter() {
-    if (counter >= 1) {
-      setCounter(counter - 1);
-    }
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Teste title="Titulo1-Teste"/>
-      <Teste title="Titulo2-Teste"/>
-      <h2>Contador = {counter}</h2>
-      <button onClick={incrementCounter}>Incrementar</button>
-      <button onClick={decrementCounter}>Decrementar</button>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/> 
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/> 
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47460478?s=460&v=4" alt="Maicon Mares"/>
+              <div className="user-info">
+                <strong>Maicon Mares</strong>
+                <span>HTML, CSS, Javascript</span>
+              </div>
+            </header>
+            <p>I'm passionate about all things involving technology. Software Engineering, UnB-FGA.
+            </p>
+            <a href="https://github.com/MaiconMares">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47460478?s=460&v=4" alt="Maicon Mares"/>
+              <div className="user-info">
+                <strong>Maicon Mares</strong>
+                <span>HTML, CSS, Javascript</span>
+              </div>
+            </header>
+            <p>I'm passionate about all things involving technology. Software Engineering, UnB-FGA.
+            </p>
+            <a href="https://github.com/MaiconMares">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47460478?s=460&v=4" alt="Maicon Mares"/>
+              <div className="user-info">
+                <strong>Maicon Mares</strong>
+                <span>HTML, CSS, Javascript</span>
+              </div>
+            </header>
+            <p>I'm passionate about all things involving technology. Software Engineering, UnB-FGA.
+            </p>
+            <a href="https://github.com/MaiconMares">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/47460478?s=460&v=4" alt="Maicon Mares"/>
+              <div className="user-info">
+                <strong>Maicon Mares</strong>
+                <span>HTML, CSS, Javascript</span>
+              </div>
+            </header>
+            <p>I'm passionate about all things involving technology. Software Engineering, UnB-FGA.
+            </p>
+            <a href="https://github.com/MaiconMares">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
