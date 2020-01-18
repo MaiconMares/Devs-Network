e4f6886c8e01e78d104e006759ba63ea91f235f9
Maicon Mares <maiconmaresunb@gmail.com>
2020-01-18 16:01:04 -0300

    Adicionando campo de buscas.

--- mobile/src/Pages/Main.js
+++ mobile/src/Pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; //Icones do expo
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -38,21 +39,35 @@ function Main({ navigation }) {
     }
 
     return ( 
-        <MapView style={styles.attributes} initialRegion={currentRegion}>
-            <Marker coordinate={{ latitude: -15.6480925, longitude: -47.8271517}}>
-                <Image style={styles.avatar} source={ {uri: 'https://avatars3.githubusercontent.com/u/47460478?s=460&v=4'}}></Image>
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'MaiconMares' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Maicon Mares</Text>
-                        <Text style={styles.devBio}>I'm passionate about all things involving Tech.</Text>
-                        <Text style={styles.devTechs}>Javascript, HTML, CSS, Node.js, React Native, React JS, C++, Python</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
-    )
+        <>
+            <MapView style={styles.attributes} initialRegion={currentRegion}>
+                <Marker coordinate={{ latitude: -15.6480925, longitude: -47.8271517}}>
+                    <Image style={styles.avatar} source={ {uri: 'https://avatars3.githubusercontent.com/u/47460478?s=460&v=4'}}></Image>
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'MaiconMares' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Maicon Mares</Text>
+                            <Text style={styles.devBio}>I'm passionate about all things involving Tech.</Text>
+                            <Text style={styles.devTechs}>Javascript, HTML, CSS, Node.js, React Native, React JS, C++, Python</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}>
+                </TextInput>
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color="#fff"/>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -79,6 +94,40 @@ const styles = StyleSheet.create({
     },
     devTechs: {
         marginTop: 5,
+    }, 
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        color: '#333',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            position: 'absolute',
+            width: 4,
+            height: 4,
+        },
+        elevation: 4, //Propriedade de sombra do React
+    }, 
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     }
 })
 
--- mobile/src/Pages/Profile.js
+++ mobile/src/Pages/Profile.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-function Profile() {
-    return <></>
+function Profile(props) {
+    const githubUsername = props.navigation.getParam('github_username');
+    return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUsername}`}}/>
 }
 
 export default Profile;
